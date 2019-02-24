bf7d0b66a12bb9140047fe6a093dfa4d05349d3f
k.iluschenko <k.iluschenko@gmail.com>
2019-02-24 13:09:00 +0300

    component created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id='app'>
+    <proxy />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Proxy from './components/Proxy.vue'
 export default {
-  name: "app",
+  name: 'app',
   components: {
-    HelloWorld
+    Proxy
   }
 };
 </script>
 
 <style>
+@import  '../node_modules/bootstrap/dist/css/bootstrap.css';
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  
 }
+  html {
+    font-size: 14px;
+  }
 </style>
